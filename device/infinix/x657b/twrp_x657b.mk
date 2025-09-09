4099cb6af1d379462f9bc6c1cc847aaeba04fb56
root <root@localhost.localdomain>
2025-09-09 09:29:07 +0000

    Add twrp_x657b.mk for Infinix Smart 5 X657B

--- device/infinix/x657b/twrp_x657b.mk
+++ device/infinix/x657b/twrp_x657b.mk
@@ -1,16 +1,14 @@
-# twrp_x657b.mk - Product makefile for Infinix Smart 5 (X657B)
-
 LOCAL_PATH := device/infinix/x657b
 
-# Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := x657b
+# Inherit TWRP common config
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier
 PRODUCT_DEVICE := x657b
 PRODUCT_NAME := twrp_x657b
-PRODUCT_BRAND := infinix
+PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Smart 5
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MANUFACTURER := Infinix
+
+# Bootloader
+PRODUCT_BOARD := mt6761

1b63c90d380a556ed77b364c29db956b3488ad8a
root <root@localhost.localdomain>
2025-09-09 08:16:00 +0000

    Add device.mk and recovery.fstab for Infinix Smart 5 X657B

--- device.mk
+++ device.mk
@@ -1,8 +1,16 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Device makefile for Infinix Smart 5 (X657B)
 
-LOCAL_PATH := device/infinix/Infinix-X657B
+LOCAL_PATH := $(call my-dir)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
+
+# Inherit common TWRP stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Device properties
+PRODUCT_DEVICE := x657b
+PRODUCT_NAME := twrp_x657b
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := Infinix SMART 5
+PRODUCT_MANUFACTURER := infinix

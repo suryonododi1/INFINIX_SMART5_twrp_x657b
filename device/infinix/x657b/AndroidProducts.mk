20dc55d6fcfcad66e480030d2f049729bb9664bd
root <root@localhost.localdomain>
2025-09-09 09:23:49 +0000

    Add AndroidProducts.mk for Infinix Smart 5

---
+++ device/infinix/x657b/AndroidProducts.mk
@@ -0,0 +1,9 @@
+# AndroidProducts.mk - Infinix Smart 5 (X657B)
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_x657b.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_x657b-user \
+    twrp_x657b-userdebug \
+    twrp_x657b-eng

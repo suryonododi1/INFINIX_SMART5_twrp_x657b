9af070209db8634697ba09baa57e56ac3f427130
Dodi Suryono <suryonododi1@gmail.com>
2025-09-09 14:22:02 +0700

    Add files via upload

--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Android device tree for Infinix Infinix X657B (Infinix-X657B)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := device/infinix/Infinix-X657B
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for Infinix Infinix X657B (Infinix-X657B)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_Infinix-X657B-user
+add_lunch_combo omni_Infinix-X657B-userdebug
+add_lunch_combo omni_Infinix-X657B-eng
